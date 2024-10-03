58171c072472da6747c6f3ef4513905c173c21a4
gaoweixuan <breeze-cloud@foxmail.com>
2024-10-03 20:38:07 +0800

    fix: 优化

--- src/views/system/messages/msg/components/MsgAddOrEdit.vue
+++ src/views/system/messages/msg/components/MsgAddOrEdit.vue
@@ -191,7 +191,16 @@ defineExpose({
       </el-form-item>
     </el-form>
     <template #footer>
-      <el-button @click="visible = false">{{ t('common.cancel') }}</el-button>
+      <el-button
+        @click="
+          () => {
+            visible = false
+            loading = false
+          }
+        "
+      >
+        {{ t('common.cancel') }}
+      </el-button>
       <el-button type="primary" @click="handleMsgDataFormSubmit()">{{ t('common.confirm') }}</el-button>
     </template>
   </el-dialog>
